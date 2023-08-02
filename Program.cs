e806118471b6a20ea1ae126b857a052c9b13880e
vladaluzik <43728723+vladaluzik@users.noreply.github.com>
2023-08-02 10:32:32 +0300

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,53 @@
+//          Задача:
+// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символам. Первоначальный массив можно ввести с 
+// клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомндуется
+// пользоваться коллекциями, лучше обойтись исключительно массивами.
+//         Решение:
+string[] GetArrayStringConsole (string inConsolSimvol)
+{
+    string[] arraySimvol = new string[inConsolSimvol.Length];
+    arraySimvol = inConsolSimvol.Split(",");
+    return arraySimvol;
+}
+string[] GetArrayThreeSimvol (string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        string simvol = array[i];
+        if (simvol.Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] arrayThreeSimvol = new string[count];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        string simvol = array[i];
+        if (simvol.Length <= 3)
+        {
+            arrayThreeSimvol[j] = simvol;
+            j++;
+        }
+    }
+    return arrayThreeSimvol;
+}
+void Print (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+Console.Write("Введите набор символов через знак (,) - ");
+string inStringSimvol = Console.ReadLine();
+string[] arraySimvol = GetArrayStringConsole(inStringSimvol);
+Console.WriteLine();
+Console.Write("Введенные символы - ");
+Print(arraySimvol);
+Console.WriteLine();
+Console.Write("Введенные символы длинной меньше либо равны 3 - ");
+Print(GetArrayThreeSimvol(arraySimvol));
+Console.WriteLine();
